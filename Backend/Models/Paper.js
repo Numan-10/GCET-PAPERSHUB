71d3cf40df8d605e34783dcc1f5efcf30fc9ea29
Numan <9256fa@gmail.com>
2025-01-17 16:58:56 +0530

    Form the connection & Paper Model

---
+++ Backend/Models/Paper.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+const paperSchema = new Schema({
+  Title: {
+    type: String,
+    required: true,
+  },
+  Subject: {
+    type: String,
+    required: true,
+  },
+
+  Semester: {
+    type: String,
+    required: true,
+  },
+  Pdf: {
+    Url: String,
+    filename: String,
+    required: true,
+  },
+  Department: {
+    type: String,
+    required: true,
+  },
+  UploadedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const paperModel = mongoose.model("paper", paperSchema);
+module.exports = paperModel;
--- Backend/init/index.js
+++ Backend/init/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const Papers = require("../Models/Paper");
+const initData = require("../init/data");
 const app = express();
 
 const PORT = 3000;
@@ -16,7 +17,12 @@ main()
     console.log(err);
   });
 
-
+const initDb = async () => {
+  await Papers.deleteMany({});
+  await Papers.insertMany(initData.data);
+  console.log("Data uploaded");
+};
+initDb();
 
 app.listen(PORT, (req, res) => {
   console.log(`App listing on port ${PORT}`);
