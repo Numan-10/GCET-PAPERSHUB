79f0cd1319ece86864a7e4113ca45a0a91342888
Numan <9256fa@gmail.com>
2025-01-18 12:47:18 +0530

    Updates component done

--- Frontend/src/Components/updates/UpdatePage.jsx
+++ Frontend/src/Components/updates/UpdatePage.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import Update from "./update";
 
 function UpdatesPage() {
-  return <div>Updates</div>;
+  return (
+    <div className="container ">
+      <Update
+        msg={
+          "Lorem tem ullam tenetur perspiciatis ipsam enim. Magnam dolori isnxnsnxnxxk tfssvcd ugsv xuhsvsabbvsabs sbbhhbbbbi"
+        }
+      />
+      <Update msg={"A new unread messages!"} />
+      <Update msg={"Projects!"} />
+
+      <div className="">
+        <h2 className="text-center mt-5" style={{fontWeight:"bolder"}}>ALL CAUGHT UP!</h2>
+      </div>
+    </div>
+  );
 }
 
 export default UpdatesPage;
