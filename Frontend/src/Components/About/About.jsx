ea53e07b2ff42df6f87e9308ef777451a7c20dac
Numan <9256fa@gmail.com>
2025-01-18 15:12:02 +0530

    About Component Done

--- Frontend/src/Components/About/About.jsx
+++ Frontend/src/Components/About/About.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from "react";
 function About() {
-    return (
-        <div>About</div>
-    );
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-12 text-center mb-5">
+          <h1 style={{fontWeight:"bolder"}} className="heading " >Together Strong !</h1>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col"></div>
+        <div className="col-12 col-md-4">
+          <p className="text-center p-2">
+            Our mission is to empower students by providing a user-friendly
+            notes- sharing platform.We aim to simplify the learning process
+            ,foster collaboration,and create a supportive community.
+          </p>
+        </div>
+        <div className="col"></div>
+      </div>
+    </div>
+  );
 }
 
 export default About;
--- Frontend/src/Components/About/Social.jsx
+++ Frontend/src/Components/About/Social.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React from "react";
+
 function Social() {
-    return ( 
-        <div>Social</div>
-     );
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-12 p-1 text-center Home ">Home</div>
+        <p className="text-center mt-1 join">
+          JOIN US AND LET'S EXPLORE TOGETHER
+        </p>
+      </div>
+      <div className="row text-center ">
+        <div className="socials d-flex justify-content-center">
+          <div className="p-2">
+            <i class="fa-brands fa-github"></i> |
+          </div>
+          <div className="p-2">
+          <i class="fa-brands fa-whatsapp"></i> |
+          </div>
+          <div className="p-2">
+            <i class="fa-brands fa-discord"></i> |
+          </div>
+          <div className="p-2">
+            <i class="fa-brands fa-telegram"></i> 
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Social;
