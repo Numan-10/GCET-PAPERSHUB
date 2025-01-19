5b5d38dbced3b198e07499771e2429177b748443
Numan <9256fa@gmail.com>
2025-01-19 16:05:02 +0530

    Some Styling updates

--- Frontend/src/Components/About/About.jsx
+++ Frontend/src/Components/About/About.jsx
@@ -5,18 +5,12 @@ function About() {
       <div className="row">
         <div className="col-12 text-center mb-5">
           <h1 style={{fontWeight:"bolder"}} className="heading " >Together Strong !</h1>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col"></div>
-        <div className="col-12 col-md-4">
-          <p className="text-center p-2">
-            Our mission is to empower students by providing a user-friendly
-            notes- sharing platform.We aim to simplify the learning process
+         <p className="text-center mt-5 lh-lg ">
+            Our mission is to empower  students by providing a user-friendly
+            notes-sharing platform. We aim to simplify the learning process
             ,foster collaboration,and create a supportive community.
           </p>
         </div>
-        <div className="col"></div>
       </div>
     </div>
   );
--- Frontend/src/Components/PageNotFound.jsx
+++ Frontend/src/Components/PageNotFound.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 function PageNotFound() {
   return (
-    <div className="container">
+    <div className="container  pnf">
       <div className="row text-center mt-5 mb-5">
       <h2 className="mt-5">404 Not Found</h2>
       <p className="mb-5">Sorry, the page you are looking for doesn't exist.</p>
