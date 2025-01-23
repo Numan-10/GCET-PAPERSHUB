5f4d4ea3a715f90f72a8622b78e5c85ae5fe2d00
Numan <9256fa@gmail.com>
2025-01-23 23:10:52 +0530

    Styling added : changed About text

--- Frontend/src/Components/About/About.jsx
+++ Frontend/src/Components/About/About.jsx
@@ -4,13 +4,21 @@ function About() {
     <div className="container mt-5">
       <div className="row">
         <div className="col-12 text-center mb-5">
-          <h1 style={{fontWeight:"bolder"}} className="heading " >Together Strong !</h1>
-         <p className="text-center mt-5 lh-lg ">
-            Our mission is to empower  students by providing a user-friendly
-            notes-sharing platform. We aim to simplify the learning process
-            ,foster collaboration,and create a supportive community.
+          <h1 style={{ fontWeight: "bolder" }} className="heading ">
+            Together Strong !
+          </h1>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col"></div>
+        <div className="col-9">
+          <p className="text-center mt-2 lh-lg ">
+            Our vision is to revolutionize exam preparation by offering GCET
+            students seamless access to previous year papers, empowering them to
+            excel academically and prepare with confidence.
           </p>
         </div>
+        <div className="col"></div>
       </div>
     </div>
   );
--- Frontend/src/Components/About/Social.jsx
+++ Frontend/src/Components/About/Social.jsx
@@ -3,29 +3,49 @@ import React from "react";
 function Social() {
   return (
     <div className="container mt-5">
-      <div className="row">
-        <div className="col-12 p-1 text-center Home ">Home</div>
-        <p className="text-center mt-1 join">
-          JOIN US AND LET'S EXPLORE TOGETHER
-        </p>
+  <div className="row">
+    <div className="col-12 p-1 text-center Home">Home</div>
+    <p className="text-center mt-1 join">
+      JOIN US AND LET'S EXPLORE TOGETHER
+    </p>
+  </div>
+  <div className="row text-center p-2">
+    <div className="socials d-flex justify-content-center">
+      <div className="p-1">
+        <a
+          href="https://github.com/GCET-CSE2022"
+          target="_black"
+          className="icon-link"
+        >
+          <i className="fa-brands fa-github"></i>
+        </a>
+        <span>|</span>
       </div>
-      <div className="row text-center ">
-        <div className="socials d-flex justify-content-center">
-          <div className="p-1">
-            <i class="fa-brands fa-github"></i> |
-          </div>
-          <div className="p-1">
-          <i class="fa-brands fa-whatsapp"></i> |
-          </div>
-          <div className="p-1">
-            <i class="fa-brands fa-discord"></i> |
-          </div>
-          <div className="p-1">
-            <i class="fa-brands fa-telegram"></i> 
-          </div>
-        </div>
+      <div className="p-1">
+        <a href="" target="_black" className="icon-link">
+          <i className="fa-brands fa-whatsapp"></i>
+        </a>
+        <span>|</span>
+      </div>
+      <div className="p-1">
+        <a
+          href="https://discord.gg/y44tDqVYdH"
+          target="_black"
+          className="icon-link"
+        >
+          <i className="fa-brands fa-discord"></i>
+        </a>
+        <span>|</span>
+      </div>
+      <div className="p-1">
+        <a href="" target="_black" className="icon-link">
+          <i className="fa-brands fa-telegram"></i>
+        </a>
       </div>
     </div>
+  </div>
+</div>
+
   );
 }
 
