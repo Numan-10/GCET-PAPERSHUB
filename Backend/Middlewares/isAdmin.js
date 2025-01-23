8fc13430a978084b0ee6d2d5fc10a1aac835d110
Numan <9256fa@gmail.com>
2025-01-23 20:16:24 +0530

    All working

--- Backend/Middlewares/isAdmin.js
+++ Backend/Middlewares/isAdmin.js
@@ -1,16 +1,13 @@
 module.exports.isAdmin = (req, res, next) => {
   const adminEmail = "9256fa@gmail.com";
-  //   console.log("currUser " + req.user);
   const currUser = req.user;
   if (req.user && req.user.email === adminEmail) {
     res.json({ status: true, user: currUser.username, email: currUser.email });
   } else {
-    return res
-      .status(403)
-      .json({
-        status: false,
-        message: "Access denied. Admins only.",
-        email: currUser.email,
-      });
+    return res.status(403).json({
+      status: false,
+      message: "Access denied. Admins only.",
+      email: currUser.email,
+    });
   }
 };
