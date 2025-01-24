326f44af4975342e45b2f1ff93b94babc0102587
Numan <9256fa@gmail.com>
2025-01-24 16:34:31 +0530

    Updated Links

--- Backend/index.js
+++ Backend/index.js
@@ -20,7 +20,10 @@ const Url = process.env.MONGO_URL;
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://qpsrqwh0-5173.inc1.devtunnels.ms",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
