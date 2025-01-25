ff5e78b501fbada4969d43c8c9c3281539f19b20
Numan <9256fa@gmail.com>
2025-01-25 22:49:36 +0530

    Fixing path issues-1

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,22 +1,19 @@
 import { createRoot } from "react-dom/client";
-import About from "./Components/About/AboutPage.jsx";
-// import Contributors from "./Components/Contributors/ContributePage.jsx";
-import Home from "./Components/Home/HomePage.jsx";
-import Landing from "./Components/Landing/Gcet.jsx" 
-// import Gcet from "./Components/Landing/Gcet.jsx";
-// import Updates from "./Components/Updates/UpdatePage.jsx";
+import AboutPage from "./Components/About/AboutPage.jsx";
+import Gcet from "./Components/Landing/Gcet.jsx" 
 import UpdatePage from "./Components/Update/UpdatePage.jsx";
 import Navbar from "./Components/Navbar.jsx";
 import Footer from "./Components/Footer.jsx";
 import PageNotFound from "./Components/PageNotFound.jsx";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router";
-import Upload from "./Components/Upload/UploadPage.jsx";
+import UploadPage from "./Components/Upload/UploadPage.jsx";
 import Login from "./Components/Login/Login.jsx";
 import Signup from "./Components/Signup/Signup.jsx";
 // import Content from "./Components/Content/Content.jsx";
 import ContentPage from "./Components/Content/ContentPage.jsx";
 import "./index.css";
 import SubDetails from "./Components/Details/SubDetails.jsx";
+// import ContributePage from "./Components/contributors/contributePage.jsx";
 import ContributePage from "./Components/Contributors/ContributePage.jsx";
 // import ContributePage from "./Components/contributors/contributePage.jsx";
 
@@ -38,13 +35,13 @@ createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Layout>
       <Routes>
-        <Route path="/" element={<Landing />} />
+        <Route path="/" element={<Gcet />} />
         <Route path="/home" element={<Home />} />
         <Route path="/home/:id" element={<SubDetails />} />
         <Route path="/contributors" element={<ContributePage />} />
         <Route path="/updates" element={<UpdatePage />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/upload" element={<Upload />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/upload" element={<UploadPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/content" element={<ContentPage />} />
