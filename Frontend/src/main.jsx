0064ecc5119a256f62d89c484334667af140d485
Numan <9256fa@gmail.com>
2025-01-28 15:24:22 +0530

    Fixing Cookies!

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -6,14 +6,14 @@ import Navbar from "./Components/Navbar.jsx";
 import Footer from "./Components/Footer.jsx";
 import PageNotFound from "./Components/PageNotFound.jsx";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router";
-import UploadPage from "./Components/Upload/UploadPage.jsx";
+import UploadPage from "./Components/upload/UploadPage.jsx";
 import HomePage from "./Components/Home/HomePage.jsx";
 import Login from "./Components/Login/Login.jsx";
 import Signup from "./Components/Signup/Signup.jsx";
 import ContentPage from "./Components/Content/ContentPage.jsx";
 import "./index.css";
 import SubDetails from "./Components/Details/SubDetails.jsx";
-import ContributePage from "./Components/Contributors/ContributePage.jsx";
+import ContributePage from "./Components/contributors/ContributePage.jsx";
 import { CookiesProvider } from "react-cookie";
 
 function Layout({ children }) {
