55624535e625c6d0940508fbfc5caaa8da176d5f
Numan <9256fa@gmail.com>
2025-01-28 14:40:26 +0530

    Fixing Cookies!

--- Backend/controllers/AuthController.js
+++ Backend/controllers/AuthController.js
@@ -16,7 +16,7 @@ module.exports.Signup = async (req, res, next) => {
     const token = createSecretToken(user._id, user.username);
     res.cookie("token", token, {
       withCredentials: true,
-      httpOnly: true,
+      // httpOnly: true,
       secure: process.env.NODE_ENV === "production",
       sameSite: "none",
       maxAge: 12 * 60 * 60 * 1000,
@@ -45,7 +45,7 @@ module.exports.Login = async (req, res, next) => {
     const token = createSecretToken(user._id, user.username);
     res.cookie("token", token, {
       withCredentials: true,
-      httpOnly: true,
+      // httpOnly: true,
       secure: process.env.NODE_ENV === "production",
       sameSite: "none",
       maxAge: 12 * 60 * 60 * 1000,
