e5bdc98066101ab53e8ba6e49e371f753590a869
Numan <9256fa@gmail.com>
2025-02-28 14:28:25 +0530

    Update contributors

--- Frontend/src/Components/contributors/ContributePage.jsx
+++ Frontend/src/Components/contributors/ContributePage.jsx
@@ -6,9 +6,6 @@ function ContributePage() {
       <div className="container-fluid d-flex justify-content-center align-items-center text-center mt-5">
         <div className="row">
           <Contribute name={"Numan"} role={"Full-Stack developer"} src={"/Assets/numan.png"}/>
-          <Contribute name={"Yaseen"} role={"Project_Manager"} src={"/Assets/yaseen.jpg"} />
-          <Contribute name={"Mubashir"} role={"FrontEnd developer"} src={"/Assets/mubashir.jpg"}/>
-          <Contribute name={"Faheem"} role={"UI/UX"} src={"/Assets/Faheem.jpg"} />
         </div>
       </div>
     </>
