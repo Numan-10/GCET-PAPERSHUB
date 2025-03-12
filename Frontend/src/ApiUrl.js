1ec85dcafe4469db704bb709ae9a1fcb0e2ed5fc
Numan <9256fa@gmail.com>
2025-03-12 23:42:26 +0530

    Testing features - 2

--- Frontend/src/ApiUrl.js
+++ Frontend/src/ApiUrl.js
@@ -1,7 +1,6 @@
-const API_BASE_URL =
-  //  import.meta.env.VITE_APP_BACKEND_URL;
-  import.meta.env.mode === "production"
-    ? import.meta.env.VITE_APP_BACKEND_URL
-    : import.meta.env.VITE_APP_LOCAL_BACKEND_URL;
+const API_BASE_URL = import.meta.env.VITE_APP_BACKEND_URL;
+// import.meta.env.mode === "production"
+//   ? import.meta.env.VITE_APP_BACKEND_URL
+//   : import.meta.env.VITE_APP_LOCAL_BACKEND_URL;
 
 export default API_BASE_URL;
