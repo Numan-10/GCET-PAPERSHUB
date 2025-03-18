28e593355efff673ece0645439addb550a4d1d21
Numan <9256fa@gmail.com>
2025-03-18 22:43:51 +0530

    Updates section msgs updated: hard Coded

--- Frontend/src/Components/Update/UpdatePage.jsx
+++ Frontend/src/Components/Update/UpdatePage.jsx
@@ -5,15 +5,17 @@ function UpdatePage() {
   return (
     <div className="container  mb-5">
       <Update
-        msg={
-          "Lorem tem ullam tenetur perspiciatis ipsam enim. Magnam dolori isnxnsnxnxxk tfssvcd ugsv xuhsvsabbvsabs sbbhhbbbbi"
-        }
+        msg={"📌 Don't forget to check out the latest semester notes uploaded!"}
       />
-      <Update msg={"A new unread messages!"} />
-      <Update msg={"Projects!"} />
+      <Update
+        msg={"🛠️ If something breaks, just refresh… or pretend it’s a feature."}
+      />
+      <Update msg={"🎓 GCET KMR students, this one's for you!"} />
 
       <div className="">
-        <h2 className="text-center mt-5 mb-5" style={{fontWeight:"bolder"}}>ALL CAUGHT UP!</h2>
+        <h2 className="text-center mt-5 mb-5" style={{ fontWeight: "bolder" }}>
+          ALL CAUGHT UP!
+        </h2>
       </div>
     </div>
   );
